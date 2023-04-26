59266eaff8de17c32bc4fb22b206003fae008310
DimaFX <dm.mokrousoff@yandex.ru>
2023-04-27 01:06:35 +0300

    lesson_2

--- blog/app.py
+++ blog/app.py
@@ -1,8 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+from blog.user import views
+from blog.report import views
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return 'Hello from Flask!'
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+""" def create_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(views.user)
+    app.register_blueprint(views.report) """
